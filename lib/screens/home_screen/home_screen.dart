f366f5a610008c43935ff8427ff9e1631902e163
ssktelegram <ssktelegram@gmail.com>
2022-12-16 02:47:25 +0530

    api added

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,3 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:cryptoapp/models/coin.dart';
+import 'package:cryptoapp/screens/coinsScreen.dart';
+import 'package:cryptoapp/screens/home_screen/components/coin_card_design.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -6,17 +12,27 @@ import 'components/balance_card/balance_card.dart';
 import 'components/favorites/favorites.dart';
 import 'components/portfolio/portfolio.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
- 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             Row(
               children: [
                 Column(
@@ -40,7 +56,6 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                
               ],
             ),
             const SizedBox(height: 36),
@@ -49,6 +64,41 @@ class HomeScreen extends StatelessWidget {
             const Portfolio(),
             const SizedBox(height: 36),
             const Favorites(),
+            Text("All Coins"),
+            const SizedBox(height: 36),
+            // const SliverToBoxAdapter(child: TittleBar()),
+FloatingActionButton(onPressed: (){
+  Navigator.of(context)
+                                            .push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        AllCoinsScreen()),
+                                                );
+})
+            
+
+            //     SliverToBoxAdapter(
+            //       child: SizedBox(
+            //           // width: double.infinity,
+            //           // height: MediaQuery.of(context).size.height / 1.3,
+            //           child: ListView.builder(
+            //             scrollDirection: Axis.vertical,
+            //             itemCount: coinList.length,
+            //             itemBuilder: (context, index) {
+            //               return SingleChildScrollView(
+            //                 child: CoinCardDesign(
+            //                   name: coinList[index].name,
+            //                   symbol: coinList[index].symbol,
+            //                   imageUrl: coinList[index].imageUrl,
+            //                   price: coinList[index].price.toDouble(),
+            //                   change: coinList[index].change.toDouble(),
+            //                   changePercentage:
+            //                       coinList[index].changePercentage.toDouble(),
+            //                 ),
+            //               );
+            //             },
+            //           )),
+            // )
           ],
         ),
       ),
